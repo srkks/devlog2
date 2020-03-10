5ce1a6d4887032ccb0d20e7c462ee74e72941d4f
srkks <xmflslxl2*/>
2020-03-10 21:12:52 +0900

    스프링 시큐리티 전까지

--- src/main/java/com/devlog/DevlogApplication.java
+++ src/main/java/com/devlog/DevlogApplication.java
@@ -2,7 +2,9 @@ package com.devlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing @들을 모두 활성화
 @SpringBootApplication
 public class DevlogApplication {
 
